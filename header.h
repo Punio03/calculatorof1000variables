9b778562510c1a1deb642391076d6a44693fe6c6
Jakub Malczak <118765990+Punio03@users.noreply.github.com>
2023-01-26 23:09:54 +0100

    more readable function names

--- header.h
+++ header.h
@@ -3,31 +3,46 @@
 #include <ctype.h>
 #include <string.h>
 #include <math.h>
+#include <stdbool.h>
+
+/*errors
+1 - Allocation error!
+2 - Wrong expression!
+3 - One of the values from the expressions is nan!
+4 - Circular references!
+5 - Undefined variable!
+6 - Wrong variable assignment!
+7 - Redeclaration of a variable!
+*/
+
+static char unary[8][3] = {"sin","cos","abs","tan","log","exp","sqr", "~"};
+static char binary[5] = {'+', '-', '/', '*', '^'};
 
 //parsing
-int is_f1(char c1, char c2, char c3);
-int is_f2(char c);
+int is_unary(char c1, char c2, char c3);
+int is_binary(char c);
 int priority(char c);
-void RPN(char* s);
+void RPN(char* bin);
 
 //expression_tree
-typedef struct trie {
-    char function[3];
+typedef struct tree {
+    char function[4];
     long double val;
-    struct trie* left;
-    struct trie* right;
-} trie;
+    struct tree* left;
+    struct tree* right;
+} tree;
 typedef struct stack {
     struct stack* prev;
     struct stack* next;
-    struct trie* val;
+    struct tree* val;
 } stack;
-stack* init_stack(trie* tree);
-void add_to_stack(stack* stos,trie* tree);
-void add_to_stack_f1(stack* stos,trie* tree);
-void add_to_stack_f2(stack* stos,trie* tree);
-trie* leaves(char* bin);
-trie* function12(char* bin);
-trie* exp_tree(char* expression);
+stack* init_stack(tree* t);
+void add_to_stack(stack* s,tree* t);
+void add_to_stack_unary(stack* s,tree* t);
+void add_to_stack_binary(stack* s,tree* t);
+tree* numbers(char* bin);
+tree* function_variable(char* bin);
+tree* exp_tree(char* expression);
 
-long double count(trie* tree);
+//evaluate_expression
+long double evaluate(tree* t, int len, bool* check, struct tree** arr);
